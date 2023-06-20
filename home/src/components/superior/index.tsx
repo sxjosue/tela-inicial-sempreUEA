e54a68069bf74f6f0409119fd97439820c48df38
Josué Souza <99757018+josuesxz@users.noreply.github.com>
2023-06-19 23:56:39 -0300

    Criação e inicio do estruturamento do titulo

---
+++ home/src/components/superior/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";  //quais os componentes quero usar
+
+export default function Title(){
+    return(
+        <View>
+            <Text>SempreUEA</Text> 
+        </View>
+    );
+}
