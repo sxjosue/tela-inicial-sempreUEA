1a29fa758250936f640ddc5620ee79fa3d30574d
sxjosue <99757018+josuesxz@users.noreply.github.com>
2023-06-28 13:15:21 -0400

    inserção da parte superior  e estruturamento dos botões 28.06_1
    Falta estilizar, algumas coisas...seguindo o modelo vc percebe qual é o b.o.

---
+++ home/src/components/THEMES/homeTheme.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View,} from 'react-native';
+
+export const HomeTheme = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        height: 100,
+        padding: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    },
+    colorButton:{
+        backgroundColor: "#F9F9F9",
+    }
+  }); 
--- home/src/components/screens/Minha_rede.tsx
+++ home/src/components/screens/Minha_rede.tsx
@@ -1,17 +1,51 @@
 import { setStatusBarBackgroundColor } from "expo-status-bar";
 import { alignPropType } from "react-bootstrap/esm/types";
 import React from "react-native";
-import { View, Text } from "react-native";
+import { View, Text, ImageComponent, Button} from "react-native";
+import {MaterialIcons, FontAwesome5, AntDesign} from "@expo/vector-icons";
+import {HomeTheme} from "../THEMES/homeTheme"
 import {styles} from  "../THEMES/defaultTheme"
 
 
 export default function MinhaRede(){
     return(
         <View style={styles.container}>
-            <>
-                <Text>Sempre UEA</Text>
-                <Text>Perfil</Text>
-            </>
+            
+            <View style={HomeTheme.container}>
+                <MaterialIcons name="people" size={22}/>
+                <Text>Minha rede</Text>
+                <AntDesign name="filter" size={22}/>
+                <AntDesign name="search1" size={22}/>
+            </View>
+
+                <View>
+                    <Button
+                        title="Informações"
+                    />
+                    <Button
+                        title="Minha Turma"
+                    />
+                    <Button
+                        title="Docentes"
+                    />
+                </View>
+                <View>
+                    <Button
+                        title="Coordenação"
+                    />
+                    <Button
+                        title="Secretaria"
+                    />
+                    <Button
+                        title="Grade"
+                    />
+                </View>
+                <View>
+                    <Button
+                        title="Calendário"
+                    />
+            </View>
+
         </View>
     );
 }
