4a2a03b7c82e48c4420401714d1d65ae816f43be
sxjosue <133665545+sxjosue@users.noreply.github.com>
2023-07-25 12:25:27 -0400

    estilando aos poucos os componentes

--- home/src/components/screens/minhaRede/style.ts
+++ home/src/components/screens/minhaRede/style.ts
@@ -1,17 +1,15 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 // import { THEME } from '../../themes/themes';
 
 const styles = StyleSheet.create({
 
         background: { /* View principal que engloba toda a página */
-
            //     backgroundColor: THEME.background,
                 width: '100%',
                 height: '100%',
                 alignItems: 'flex-start',
                 flex: 1,
                 backgroundColor: '#F9F9F9',
-
         },
 
         widthdiv: { /* View que delimita largura da página */
@@ -19,19 +17,35 @@ const styles = StyleSheet.create({
                 width: '80%',
                 height: '100%',
         },
-
-        barra:{
+// ---------------------------------------------------
+        barra:{ //estilixzando o box barra
                 flexDirection: 'row',
                 paddingTop: 130, 
-                backgroundColor: '#B0C4DE'
+                backgroundColor: '#D3D3D3',
         },
-
-        button:{
+        styleTextbarra: {       //estilizando os conteúdos da barra
+                color:'#1D5E24',
+                fontSize: 24,
+                fontFamily: 'Roboto',
+                fontWeight: 'bold',
+        },
+// -------------------------------------------
+        boxbutton:{
                 paddingTop: 73, //196
                 flexDirection: 'row',
                 fex: 1,
-                backgroundColor: '#3498db', 
-                color: '#fff'      
+                backgroundColor: '#1D5E24',     // cor verde do layout do do sempre
+        },
+        bottonstyle: {
+                alignItems: 'center',
+                backgroundColor: '#F9F9F9',
+                padding: 10,
+                borderRadius: 22,
+                marginTop: 10,
+        },
+        buttonText: {
+                fontSize: 18,
+                color: '#797979',
         },
 
 });
